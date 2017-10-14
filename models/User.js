657d35f2623ec627ab5af682c7263c60b2e5ae28
Bernard Bado <bado.bernard94@gmail.com>
2017-10-14 09:31:45 +0200

    Saving user

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// No need to export mongoose code. Mongoose can be confused when trying
+// to create multiple models with same name
 const userSchema = new Schema({
   googleId: String
 });
--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,21 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-  console.log(accessToken);
-  console.log(refreshToken);
-  console.log(profile);
+  User.findOne({googleId: profile.id})
+    .then((existingUser) => {
+      if (existingUser) {
+        // Already have a user
+      } else {
+        new User({googleId: profile.id}).save();
+      }
+    })
 }));
