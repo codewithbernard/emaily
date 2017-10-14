c423d6d3e125f8ef5397ee678e4b27d7129bbbad
Bernard Bado <bado.bernard94@gmail.com>
2017-10-14 10:20:52 +0200

    Adding logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,18 @@
 const passport = require('passport');
 
-module.exports = (app) => {  
+module.exports = (app) => {
   app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
   }));
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get('/api/currentUser', (req, res) => {
+    res.send(req.user);
+  });
 };
