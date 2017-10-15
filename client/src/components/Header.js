39ef8c68e17c11d2a1fc922f53f891a0934ff097
Bernard Bado <bado.bernard94@gmail.com>
2017-10-15 19:02:32 +0200

    Adding link to logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -17,7 +18,7 @@ class Header extends Component {
       return(
           <nav>
             <div className="nav-wrapper">
-              <a href="#" className="left brand-logo">Emaily</a>
+              <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">Emaily</Link>
               <ul id="nav-mobile" className="right hide-on-med-and-down">
                 {this.renderContent()}
               </ul>
