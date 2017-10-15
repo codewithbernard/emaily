ea766b600b278d56abb72d21bdb171e28681c3ce
Bernard Bado <bado.bernard94@gmail.com>
2017-10-15 18:00:02 +0200

    REfactoring fetchUser

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
-  axios.get('/api/currentUser');
-}
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get('/api/currentUser')
+    dispatch({ type: FETCH_USER, payload: res });
+};
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,9 @@
+import { FETCH_USER } from '../actions/types';
+
 export default (state = {}, action) => {
   switch(action.type) {
+    case FETCH_USER:
+      return state;
     default:
       return state;
   }
