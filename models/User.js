c67e26a4366ea3341c02f1c02d403022e4edf6c0
Bernard Bado <bado.bernard94@gmail.com>
2017-10-16 18:47:39 +0200

    Adding credits to user

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 // No need to export mongoose code. Mongoose can be confused when trying
 // to create multiple models with same name
 const userSchema = new Schema({
-  googleId: String
+  googleId: String,
+  credits: {type: Number, default: 0}
 });
 
 mongoose.model('users', userSchema);
