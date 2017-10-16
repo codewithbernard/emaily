9884883b989699f7fa5627a5e9e5f8d74d4cddf0
bernard <bado.bernard94@gmail.com>
2017-10-16 14:17:07 +0200

    Changing style of pay button

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -8,7 +8,11 @@ class Payments extends Component {
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+        name="Emaily"
+        description="$5 for 5 email credits"
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
