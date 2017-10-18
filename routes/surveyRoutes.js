fa2e1e7424d607910b54d8c612603fea24e831fb
Bernard Bado <bado.bernard94@gmail.com>
2017-10-18 17:12:33 +0200

    Polishing survey routes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -5,7 +5,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, (req, res) => {
+  app.post('/api/surveys', requireLogin, async (req, res) => {
     const { title, subject, body, recipients} = req.body;
     const survey = new Survey({
       title,
@@ -17,6 +17,14 @@ module.exports = app => {
     });
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+    try {
+    await mailer.send();
+    await survey.save();
+    req.user.credits -= 1;
+    const user = await req.user.save();
+    res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 }
