8aa896e43edd3f7361a8f2569d677e5195f9f0bb
bernard <bado.bernard94@gmail.com>
2017-10-18 07:29:27 +0200

    Creating survey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,19 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+
+const Survey = mongoose.model('surveys');
 
 module.exports = app => {
   app.post('/api/surveys', requireLogin, (req, res) => {
+    const { title, subject, body, recipients} = req.body;
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
 
   });
 }
