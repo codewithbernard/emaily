7aa3c619b5fd54cec2f763f2a407e88fcd731e71
Bernard Bado <bado.bernard94@gmail.com>
2017-10-21 17:39:57 +0200

    Adding form fields

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export default () => {
+export default ({ input, label }) => {
   return(
     <div>
-      Survey Field
+      <label>{label}</label>
+      <input {...input}/>
     </div>
   );
 }
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -8,8 +8,27 @@ class SurveyForm extends Component {
     return(
       <div>
         <Field
+          label="Survey Title"
           type="text"
           name="title"
+          component={SurveyField}
+        />
+        <Field
+          label="Subject Line"
+          type="text"
+          name="subject"
+          component={SurveyField}
+        />
+        <Field
+          label="Email Body"
+          type="text"
+          name="body"
+          component={SurveyField}
+        />
+        <Field
+          label="Recipient List"
+          type="text"
+          name="emails"
           component={SurveyField}
         />
       </div>
