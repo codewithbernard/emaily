870a499ce5bdb62ba6378c334793b4e3fd644d7c
Bernard Bado <bado.bernard94@gmail.com>
2017-10-22 10:51:51 +0200

    Using lodash chain

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -14,20 +14,19 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, ({ email, url }) => {
-      const pathname = new URL(url).pathname;
-      const p = new Path('/api/surveys/:surveyId/:choice');
-      const match = p.test(pathname);
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    const events = _.chain(req.body)
+    .map(({ email, url }) => {
+      const match = p.test(new URL(url).pathname);
       if (match) {
-        return {
-          email,
-          surveyId: match.surveyId,
-          choice: match.choice
-        }
+        return { email, surveyId: match.surveyId, choice: match.choice }
       }
-    });
-    const compactEvents = _.compact(events);
-    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+    })
+    .compact()
+    .uniqBy('email', 'surveyId')
+    .value();
+
+    res.send({});
   });
 
   app.post('/api/surveys', requireLogin, async (req, res) => {
